7d86c9f9eb0e4e1c573da749b5df090dbae5144e
Tess Neau <tessneau@gmail.com>
2019-03-19 19:41:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function theBeatlesPlay(musicians,instruments) {
   var whoPlaysWhat=[]
   
   for (var count = 0; count < musicians.length; count++) {
-    whoPlaysWhat[count] = `${musicians[count]} plays ${instruments[count]}`;
+    whoPlaysWhat[count] = `${musicians[count]} plays ${instruments[count]}`
   }
   
-  return whoPlaysWhat;
+  return whoPlaysWhat
 }
